1d05c1ab51577c60acd8d4ba96c43a84c212cdcb
Lindan Maulana <lindanstemga@gmail.com>
2026-02-15 15:23:58 +0700

    refactor: App controller

--- src/modules/apps/app.controller.ts
+++ src/modules/apps/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from '@/modules/apps/app.service';
-import { ResponseMessage } from '@/common/decorators/response-message.decorator';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
@@ -9,14 +8,5 @@ export class AppController {
 	@Get()
 	getHello(): string {
 		return this.appService.getHello();
-	}
-
-	@Get('ip')
-	@ResponseMessage('Ip user', 'GET')
-	getIp(@Req() req: any) {
-		return {
-			ipDariRequest: req.ip,
-			ipDariHeader: req.headers['x-forwarded-for'],
-		};
 	}
 }
